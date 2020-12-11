5bda31188232c5f9afd747356b34017b3465e279
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2020-12-11 15:12:52 +0100

    dia1 - reto 1 completado

--- Soluciones/src/dia01/dia1.java
+++ Soluciones/src/dia01/dia1.java
@@ -8,15 +8,33 @@ public class dia1 {
 	
 	public static void main(String [] args) {
 		
-		File f = new File("/Soluciones/src/dia01/input.txt");
+		File f = new File("C:\\Users\\lugud\\git\\AdventOfCode2020\\Soluciones\\src\\dia01\\input.txt");
 		Scanner reader;
+		int contador = 0;
 		try {
 			reader = new Scanner(f);
-			int valor = Integer.parseInt(reader.nextLine());
-			System.out.println(valor);
+			int [] valor = new int [(int)f.length()];
+			while(reader.hasNextLine()) {
+				int v = Integer.parseInt(reader.nextLine());
+				valor[contador]=v;
+				contador++;
+				System.out.println(v);
+			}
+			
+			for(int i=0; i<valor.length; i++) {
+				for(int j=0; j<valor.length; j++) {	
+					if(valor[i]+valor[j]==2020) {
+						System.out.println(valor[i]+" + "+valor[j]+" = "+ (valor[i]+valor[j]));
+						System.out.println("Su multiplicacion vale: "+(valor[i]*valor[j]));
+					}	
+				}
+			}
+			
+			reader.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
 		
 
 	}
