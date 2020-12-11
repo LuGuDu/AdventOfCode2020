f0d8a6931ea78ffdf42566c2807509322b51ac08
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2020-12-11 15:27:21 +0100

    comienzo dia 2 - reto 1

--- Soluciones/src/dia02/dia2.java
+++ Soluciones/src/dia02/dia2.java
@@ -1,5 +1,19 @@
 package dia02;
 
-public class dia2 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class dia2 {
+	
+	File f = new File("C:\\Users\\lugud\\git\\AdventOfCode2020\\Soluciones\\src\\dia02\\input.txt");
+	Scanner reader;
+	try
+	{
+		while (reader.hasNextLine()) {
+			int v = Integer.parseInt(reader.nextLine());
+		}
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	}
 }
