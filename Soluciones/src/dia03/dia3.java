5a132c95dc07e9ff35d4b0ab7e3cfd930a5dfc5f
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2020-12-11 23:07:03 +0100

    completado dia 3 - reto 1

--- Soluciones/src/dia03/dia3.java
+++ Soluciones/src/dia03/dia3.java
@@ -1,5 +1,34 @@
 package dia03;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class dia3 {
 
+	public static void main (String [] args) {
+		File f = new File("C:\\Users\\lugud\\git\\AdventOfCode2020\\Soluciones\\src\\dia03\\input.txt");
+		int contador=0, c=0;
+		int i=0;
+		try {
+			
+			Scanner reader = new Scanner(f);
+			while (reader.hasNextLine()) {
+				String s = reader.nextLine();
+				String s1 = "";
+				for(int j=0; j<=c; j++) {
+					s1 = s1 + s;
+				}				
+				if(s1.charAt(i)=='#') {
+					contador++;
+				}
+				c++;
+				i=i+3;
+			}			
+			reader.close();
+			System.out.println("Contador: "+contador);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
