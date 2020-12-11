18b3504d946b144518c22af091cafd9ec48ab0fa
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2020-12-11 15:16:53 +0100

    dia 1 - reto 2 completado / y mejoras en el 1

--- Soluciones/src/dia01/dia1.java
+++ Soluciones/src/dia01/dia1.java
@@ -5,37 +5,58 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class dia1 {
-	
-	public static void main(String [] args) {
-		
+
+	public static void main(String[] args) {
+
 		File f = new File("C:\\Users\\lugud\\git\\AdventOfCode2020\\Soluciones\\src\\dia01\\input.txt");
 		Scanner reader;
 		int contador = 0;
+		boolean solucion = false;
 		try {
 			reader = new Scanner(f);
-			int [] valor = new int [(int)f.length()];
-			while(reader.hasNextLine()) {
+			int[] valor = new int[(int) f.length()];
+			while (reader.hasNextLine()) {
 				int v = Integer.parseInt(reader.nextLine());
-				valor[contador]=v;
+				valor[contador] = v;
 				contador++;
-				System.out.println(v);
 			}
-			
-			for(int i=0; i<valor.length; i++) {
-				for(int j=0; j<valor.length; j++) {	
-					if(valor[i]+valor[j]==2020) {
-						System.out.println(valor[i]+" + "+valor[j]+" = "+ (valor[i]+valor[j]));
-						System.out.println("Su multiplicacion vale: "+(valor[i]*valor[j]));
-					}	
+
+			/** Reto 1 **/
+			for (int i = 0; i < valor.length; i++) {
+				for (int j = 0; j < valor.length; j++) {
+					if (valor[i] + valor[j] == 2020) {
+						System.out.println(valor[i] + " + " + valor[j] + " = " + (valor[i] + valor[j]));
+						System.out.println("Su multiplicacion vale: " + (valor[i] * valor[j]));
+						solucion = true;
+						break;
+					}
 				}
+				if(solucion) break;
 			}
+
+			solucion = false;
 			
+			/** Reto 2 **/
+			for (int i = 0; i < valor.length; i++) {
+				for (int j = 0; j < valor.length; j++) {
+					for (int k = 0; k < valor.length; k++) {
+						if (valor[i] + valor[j] + valor[k] == 2020) {
+							System.out.println(valor[i] + " + " + valor[j] + " + " + +valor[k] + " = "
+									+ (valor[i] + valor[j] + valor[k]));
+							System.out.println("Su multiplicacion vale: " + (valor[i] * valor[j] * valor[k]));
+							solucion = true;
+							break;
+						}
+					}
+					if(solucion)break;
+				}
+				if(solucion)break;
+			}
+
 			reader.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
-		
 
 	}
 }
