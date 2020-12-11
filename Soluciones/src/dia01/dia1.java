d827b61b13cfd928fa38d7bdcaa09eee87fcf97f
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2020-12-11 14:56:14 +0100

    pruebas reto1

--- Soluciones/src/dia01/dia1.java
+++ Soluciones/src/dia01/dia1.java
@@ -1,5 +1,23 @@
 package dia01;
 
-public class dia1 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class dia1 {
+	
+	public static void main(String [] args) {
+		
+		File f = new File("/Soluciones/src/dia01/input.txt");
+		Scanner reader;
+		try {
+			reader = new Scanner(f);
+			int valor = Integer.parseInt(reader.nextLine());
+			System.out.println(valor);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+
+	}
 }
